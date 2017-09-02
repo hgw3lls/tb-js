49a4db1b3147b3b13ac0bb97616f9c57ddbbefbb
hgw3lls <ajyanick@buffalo.edu>
2017-09-02 09:55:55 -0400

    update

--- latest.js
+++ latest.js
@@ -1,21 +1,14 @@
 	var locations =  [
 
 
-													
+	{"type": "point", "lat": 42.900712, "lng": -78.878110, "label": "Joint between main blind and first overlook", "audio": "audio/AMB/amb_trafficdrone_129.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	{"type": "point", "lat": 42.901120, "lng":-78.878101, "label": "Far entrance to E_Boardwalk", "audio": "audio/AMB/concert-shout_195.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	{"type": "point", "lat": 42.901521, "lng": -78.878096, "label": "Aderle Trail Sign", "audio": "audio/AMB/amb_beach_waves-CU.mp3","maxdistance": "0.02","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	{"type": "point", "lat": 42.901965, "lng": -78.878073, "label": "T-intersection E_boardwalk main entrance", "audio": "audio/AMB/amb_thunder_variety_262.mp3","maxdistance": "0.018","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
 	
-	{"type": "point", "lat": 42.900712, "lng": -78.878110, "label": "First path bend toward far entrance", "audio": "audio/AMB/cello_wide-vibrato_276.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 250, "fadeouttime": 4000, "probability": 100}, 
-	
-	{"type": "point", "lat": 42.901120, "lng": -78.878101, "label": "Midway to woodpecker", "audio": "audio/JB/JB-3_mainBlind-aural.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 250, "fadeouttime": 3000, "probability": 100}, 
-	
-	{"type": "point", "lat": 42.901521, "lng": -78.878096, "label": "Midway on path to middle boardwalk entrance", "audio": "audio/SS/SS_obsessed.mp3","maxdistance": "0.002”,”volume": 0,  "fadeintime": 250, "fadeouttime": 2000, "probability": 100}, 
-	
-	{"type": "point", "lat": 42.901965, "lng": -78.878073, "label": "Fork to middle boardwalk entrance", "audio": "audio/AMB/spirit_ride_w-birds_machines.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 250, "fadeouttime": 1000, "probability": 100}, 
-	
-	{"type": "point", "lat": 42.899728, "lng": -78.878154, "label": "Far entrance main drag", "audio": "audio/PF/16_PF_vernalPool.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100} 
+	{"type": "point", "lat": 42.899728, "lng": -78.878154, "label": "Meadow Center", "audio": "audio/SB/14_SB_studyClimateBigMess.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100}												
 	
 	
-		
 	
-
-		
+			
 ];
