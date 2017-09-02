0cdc484a9c6a1b1220951d636359e528801a2842
hgw3lls <ajyanick@buffalo.edu>
2017-09-02 09:01:39 -0400

    tb app and locations

---
+++ latest.js
@@ -0,0 +1,158 @@
+	var locations =  [
+
+
+	{
+			"type": "region_array",
+			"mode": "random",
+			"lat": 42.87344808444722,
+			"lng": -78.8814926147461,
+			"label": "entrance_ambience",
+			"bounds": [{"lat":42.872575316763765,"lng":-78.88110637664795},{"lat":42.87265787639752,"lng":-78.88086497783661},{"lat":42.87274829872679,"lng":-78.88063430786133},{"lat":42.873275104445625,"lng":-78.88102054595947},{"lat":42.873070672909584,"lng":-78.88151407241821}],
+			"volume": 1,
+			"fadeintime": 1000,
+			"fadeouttime": 1000,
+			"audio_array": ["audio/AMB/cello-child-113-mix.mp3",
+				"audio/AMB/amb_bicycles.mp3"
+			]
+		},
+	
+	
+	
+	{"type": "region", "lat": 42.873047084611805, "lng": -78.8814926147461, "loop": false, "label": "bench and sign region", "audio": "audio/MV/06_MV_notNatureNatureZone.mp3", 
+		"bounds":[{"lat":42.87340877085307,"lng":-78.88141751289368},{"lat":42.873275104445625,"lng":-78.88176083564758},{"lat":42.87306281014468,"lng":-78.88160794973373},{"lat":42.87319844269904,"lng":-78.88128072023392}], 
+		"volume": 1,"fadeintime": 1000, "fadeouttime": 1000, "probability": 100},
+	
+	
+	
+		{
+			"type": "region_array",
+			"mode": "random",
+			"lat": 42.873864807005596,
+			"lng": -78.8818359375,
+			"label": "NEW_Intro_ID",
+			"bounds": [{"lat":42.87385596150881,"lng":-78.88183057308197},{"lat":42.87375669529141,"lng":-78.88210818171501},{"lat":42.87329869265625,"lng":-78.88179302215576},{"lat":42.87341270221369,"lng":-78.88150468468666}],
+			"volume": 1,
+			"fadeintime": 1000,
+			"fadeouttime": 1000,
+			"audio_array": ["audio/AJ/01_AJ_ID.mp3",
+				"audio/MV/01_MV_ID-Books.mp3",
+				"audio/PF/01_PF_ID.mp3",
+				"audio/SB/15_SB_ID.mp3",
+				"audio/SS/SS_ShelleySeidman_ID.mp3",
+				"audio/MW/MW_ID.mp3",
+				"audio/TC/01_TC_ID.mp3"
+			]
+		},
+	
+	
+		{
+			"type": "region_array",
+			"mode": "random",
+			"lat": 42.873573887780225,
+			"lng": -78.88230800628662,
+			"label": "teasle corridor region",
+			"bounds": [{"lat":42.87344808444722,"lng":-78.88227581977844},{"lat":42.87349526072715,"lng":-78.88199150562286},{"lat":42.873393045408385,"lng":-78.88196468353271},{"lat":42.87323775642707,"lng":-78.88195395469666},{"lat":42.87323579074126,"lng":-78.88195529580116},{"lat":42.87325151622601,"lng":-78.88210013508797},{"lat":42.87325643043918,"lng":-78.88222083449364},{"lat":42.87326134465197,"lng":-78.88227850198746},{"lat":42.8733498004149,"lng":-78.8822489976883}],
+			"volume": 1,
+			"fadeintime": 1000,
+			"fadeouttime": 1000,
+			"audio_array": ["audio/TC/03_TC_teasleNative.mp3",
+				"audio/11_MW_canadianThistleNest.mp3"
+			]
+		},
+	
+	
+	{"type": "point", "lat": 42.87274, "lng": -78.88234, "label": "more bees", "audio": "audio/AMB/amb_bees1.mp3","maxdistance": "0.010","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	
+	
+	
+				{"type": "region", "lat": 42.87363678935055, "lng": -78.8823401927948, "loop": false, "label": "crickets region", "audio": "audio/AMB/amb_crickets-hipass_136.mp3", 
+		"bounds":[{"lat":42.87324168779851,"lng":-78.88228118419647},{"lat":42.87323579074126,"lng":-78.88212025165558},{"lat":42.87321809956615,"lng":-78.88195663690567},{"lat":42.873126695080586,"lng":-78.88195797801018},{"lat":42.873009736455316,"lng":-78.8819807767868},{"lat":42.87302349630507,"lng":-78.88213366270065},{"lat":42.87303332476732,"lng":-78.8822677731514}], 
+		"volume": 1,"fadeintime": 1000, "fadeouttime": 1000, "probability": 100},
+	
+	
+				{"type": "region", "lat": 42.87363678935055, "lng": -78.8823401927948, "loop": false, "label": "approaching main entrance", "audio": "audio/LS/01_LS_thisWayThatWayChanges.mp3", 
+		"bounds":[{"lat":42.87419700363317,"lng":-78.88242065906525},{"lat":42.87409675513512,"lng":-78.88300001621246},{"lat":42.87361320126921,"lng":-78.88269424438477},{"lat":42.87377831764944,"lng":-78.88215377926826},{"lat":42.87396210738644,"lng":-78.88159856200218},{"lat":42.87439160319392,"lng":-78.88188689947128}], 
+		"volume": 1,"fadeintime": 1000, "fadeouttime": 1000, "probability": 100},
+		
+	
+	{"type": "point", "lat": 42.874329, "lng": -78.882587, "label": "Main Entrance", "audio": "audio/AMB/cello_sweeps_239.mp3","maxdistance": "0.02","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},													
+	{"type": "point", "lat": 42.87542, "lng": -78.88853, "label": "Offtrail Coastguard DeerHangout", "audio": "audio/AMB/amb_deerInWoodsWater_24b.mp3","maxdistance": "0.02","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},													
+	{"type": "point", "lat": 42.87517, "lng": -78.88897, "label": "Offtrail Coastguard Corner Seawall", "audio": "audio/AMB/amb_crickets-hipass_136.mp3","maxdistance": "0.02","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},													
+	{"type": "point", "lat": 42.87467, "lng": -78.88846, "label": "Seawall Lakeside Coastguard", "audio": "audio/AMB/amb_geeseOverhead_23.mp3","maxdistance": "0.02","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},													
+	{"type": "point", "lat": 42.87305, "lng": -78.88711, "label": "Seawall Lakeside Middle", "audio": "audio/AMB/cello_low-dusty_276.mp3","maxdistance": "0.03","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},													
+	{"type": "point", "lat": 42.87222, "lng": -78.88638, "label": "Seawall Lakeside Middle2", "audio": "audio/AMB/amb_gentlerain-thundercrack_265.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},													
+	{"type": "point", "lat": 42.87179, "lng": -78.88597, "label": "Seawall Lakeside Beachside", "audio": "audio/AMB/TIFFT_mucky2-wloudbirdies.mp3","maxdistance": "0.03","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	{"type": "point", "lat": 42.874181, "lng": -78.888059, "label": "gulley", "audio": "audio/AMB/Portland_Bill_F-Type_Diaphone_1993.mp3","maxdistance": "0.03","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	{"type": "point", "lat": 42.87186, "lng": -78.88407, "label": "Wilkeson Pointe Beach", "audio": "audio/SB/09_SB_multipleEcotonesUnique.mp3","maxdistance": "0.024","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},													
+	{"type": "point", "lat": 42.87143, "lng": -78.88503, "label": "Midway Seawall", "audio": "audio/LS/16_LS_marshlandFisheries.mp3","maxdistance": "0.024","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},													
+	
+	{"type": "point", "lat": 42.87165, "lng": -78.88455, "label": "On the way to Midway Seawall", "audio": "audio/AMB/amb_dike_closeUp_waves_63.mp3","maxdistance": "0.02","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},													
+	
+	{"type": "point", "lat": 42.871231, "lng": -78.885482, "label": "Cornerpoint Seawall", "audio": "audio/AMB/Portland_Bill_F-Type_Diaphone_1993.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 3000, "fadeouttime": 3000, "probability": 100},										
+	{"type": "point", "lat": 42.87212, "lng": -78.88362, "label": "Arthur Blind", "audio": "audio/AMB/cello_hi-drone_273.mp3","maxdistance": "0.02","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},											
+	{"type": "point", "lat": 42.87361, "lng": -78.88216, "label": "Between Willows", "audio": "audio/AJ/08_AJ_cleansingMarsh.mp3","maxdistance": "0.013","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},													
+	{"type": "point", "lat": 42.872961, "lng": -78.882598, "label": "Hamilton Overlook", "audio": "audio/JB/08_JB_redwingBlackbirdsWinterSounds.mp3","maxdistance": "0.023","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},													
+	{"type": "point", "lat": 42.87236, "lng": -78.88314, "label": "Midway Seawall 2", "audio": "audio/AMB/amb_thunder_variety-2_262.mp3","maxdistance": "0.026","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	{"type": "point", "lat": 42.87258, "lng": -78.88264, "label": "middle spot to wilkeson", "audio": "audio/MW/26_MW_deadTreeSculpture.mp3","maxdistance": "0.02","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},												
+	{"type": "point", "lat": 42.8729, "lng": -78.88159, "label": "sumac point", "audio": "audio/SB/11_SB_armyCorpsConvinced.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},												
+	{"type": "point", "lat": 42.87301, "lng": -78.88185, "label": "midway sumac to intersection", "audio": "audio/AMB/texture_airplane_82.mp3","maxdistance": "0.010","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},												
+	
+	{"type": "point", "lat": 42.872878, "lng": -78.882103, "label": "Path Intersection to Hamilton Overlook", "audio": "audio/SB/01_SB_sumacPoint.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},
+	
+	{
+			"type": "region",
+			"lat": 42.34398047870971,
+			"lng": -71.10525637865067,
+			"label": "connector by main blind",
+			"audio": "audio/AMB/amb_frogs_230.mp3",
+			"bounds": [{"lat":42.87483190700575,"lng":-78.88548374176025},{"lat":42.874658930882894,"lng":-78.88526916503906},{"lat":42.8748161819237,"lng":-78.88495802879333},{"lat":42.875036332707666,"lng":-78.88517260551453},{"lat":42.87490660109073,"lng":-78.88547837734222}],
+			"volume": "1",
+			"fadeintime": 1000,
+			"fadeouttime": 1000,
+			"probability": 100
+	
+		},
+	
+	{"type": "point", "lat": 42.87567, "lng": -78.88444, "label": "Midway to woodpecker", "audio": "audio/AMB/amb_dawn_chorus_train_10_3min.mp3","maxdistance": "0.02","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	{"type": "point", "lat": 42.87574, "lng": -78.88492, "label": "Path Bend Toward E_Boardwalk (woodpecker)", "audio": "audio/SS/SS_obsessed.mp3","maxdistance": "0.02","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},													
+	
+	
+	
+	{"type": "point", "lat": 42.87566, "lng": -78.88392, "label": "Big_Fork", "audio": "audio/MW/06_MW_studyCDF_contaminants.mp3","maxdistance": "0.02","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},												
+	
+	{"type": "point", "lat": 42.87517, "lng": -78.88328, "label": "Seawall History", "audio": "audio/MV/02_MV_seawallHistory.mp3","maxdistance": "0.018","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},										
+	
+	{"type": "point", "lat": 42.87486, "lng": -78.88309, "label": "Path to Adrle Trail sign", "audio": "audio/AMB/cello_wide-vibrato_276.mp3","maxdistance": "0.018","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	
+	{"type": "point", "lat": 42.875679, "lng": -78.887568, "label": "E_Boardwalk Endpoint", "audio": "audio/AMB/Portland_Bill_F-Type_Diaphone_1993.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},										
+	
+	
+	{"type": "point", "lat": 42.874678, "lng": -78.885532, "label": "E_Boardwalk Main Blind", "audio": "audio/JB/JB-3_mainBlind-aural.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},													
+	
+	
+	{"type": "point", "lat": 42.8758, "lng": -78.88705, "label": "E_Boardwalk Far Overlook2", "audio": "audio/AMB/amb_geeseOverhead_23.mp3","maxdistance": "0.026","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},													
+	
+	
+	
+	{"type": "point", "lat": 42.87601, "lng": -78.88661, "label": "E_Boardwalk Far Overlook1", "audio": "audio/JB/06_JB_fledglingSwallows.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},													
+	{"type": "point", "lat": 42.87572, "lng": -78.88538, "label": "Midway on path to middle E_boardwalk entrance", "audio": "audio/AMB/spirit_ride_w-birds_machines.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},												
+	{"type": "point", "lat": 42.87601, "lng": -78.8852, "label": "Fork to middle E_boardwalk entrance", "audio": "audio/AMB/amb_cicadas_good_254.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	{"type": "point", "lat": 42.87617, "lng": -78.88626, "label": "Far entrance main drag", "audio": "audio/JB/JB-8_nestingSwallows.mp3","maxdistance": "0.018","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	{"type": "point", "lat": 42.87527, "lng": -78.88572, "label": "First Overlook", "audio": "audio/AMB/cello-106-mix.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	{"type": "point", "lat": 42.87504, "lng": -78.88552, "label": "Joint between main blind and first overlook", "audio": "audio/AMB/amb_trafficdrone_129.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	{"type": "point", "lat": 42.87629, "lng": -78.88589, "label": "Far entrance to E_Boardwalk", "audio": "audio/AMB/concert-shout_195.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	{"type": "point", "lat": 42.8755, "lng": -78.88346, "label": "Aderle Trail Sign", "audio": "audio/AMB/amb_beach_waves-CU.mp3","maxdistance": "0.02","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	{"type": "point", "lat": 42.875573, "lng": -78.885726, "label": "T-intersection E_boardwalk main entrance", "audio": "audio/AMB/amb_thunder_variety_262.mp3","maxdistance": "0.018","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	
+	{"type": "point", "lat": 42.87537, "lng": -78.88411, "label": "Meadow Center", "audio": "audio/SB/14_SB_studyClimateBigMess.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},												
+	{"type": "point", "lat": 42.87517, "lng": -78.88444, "label": "AMB Meadow Center", "audio": "audio/AMB/amb_crickets-hipass_136.mp3","maxdistance": "0.018","volume": 0,  "fadeintime": 1000, "fadeouttime": 1000, "probability": 100},												
+	{"type": "point", "lat": 42.875828, "lng": -78.886082, "label": "E_Boardwalk_nesting", "audio": "audio/LS/20_LS_egretThankyou.mp3","maxdistance": "0.022","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},													
+	{"type": "point", "lat": 42.87626, "lng": -78.88545, "label": "Path to E_Boardwalk Far Entrance", "audio": "audio/AMB/amb_helicopterOverhead_32.mp3","maxdistance": "0.02","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},													
+	{"type": "point", "lat": 42.8749901, "lng": -78.8848319, "label": "Path to E_Boardwalk First Junction", "audio": "audio/LS/07_LS_beginToSeeLake.mp3","maxdistance": "0.02","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},													
+	
+	{"type": "point", "lat": 42.8746, "lng": -78.88284, "label": "Vernal Pool", "audio": "audio/JB/39_JB_vernalPool.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100},												
+	{"type": "point", "lat": 42.87447, "lng": -78.88314, "label": "Vernal Pool-Offtrail", "audio": "audio/PF/16_PF_vernalPool.mp3","maxdistance": "0.015","volume": 0,  "fadeintime": 250, "fadeouttime": 500, "probability": 100}		
+	
+
+		
+];
